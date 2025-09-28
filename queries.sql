f43c2b238dd19da2ef1ee10aa397f6beae8831b1
Dariya <140879007+khussainovaD@users.noreply.github.com>
2025-09-28 22:18:41 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -149,3 +149,26 @@ FROM (
     HAVING
         COUNT(o.order_id) > 1
 ) AS loyal_customer_counts;
+
+----------------
+--Какова средняя стоимость доставки (freight_value) для топ-5 категорий товаров с самым высоким средним рейтингом? (Горизонталоно столбчатая)
+SELECT
+    t.product_category_name_english AS category,
+    AVG(r.review_score) AS average_score,
+    AVG(oi.freight_value) AS average_freight_value
+FROM
+    order_items AS oi
+JOIN
+    products AS p ON oi.product_id = p.product_id
+JOIN
+    order_reviews AS r ON oi.order_id = r.order_id
+JOIN
+    product_category_name_translation AS t ON p.product_category_name = t.product_category_name
+GROUP BY
+    t.product_category_name_english
+HAVING
+    COUNT(oi.product_id) > 50
+ORDER BY
+    average_score DESC
+LIMIT 5;
+
